c20eb3e056d6a282c961560918c29d83cd9ccd8e
guips <henry_guillaume@hotmail.fr>
2016-05-25 13:26:55 +0200

    Ajout des fichiers artists, labels et concerts

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,7 @@ var gigsServices = angular.module('gigsServices', ['ngResource']);
 
 gigsServices.factory('Artist', ['$resource',
   function($resource){
-    return $resource('http://promoteapi.herokuapp.com/artists', {}, {
+    return $resource('http://promoteapi.herokuapp.com/artists?embed[]=gigs&embed[]=labels', {}, {
       query: {method:'GET', params:{artistId:'artists'}, isArray:false}
     });
 }]);
